212f44c12810b474e6084e9dc19e7518b4271d44
martynasali <70946929+martynasali@users.noreply.github.com>
2021-01-18 09:20:22 +0200

    php
    php

--- index.php
+++ index.php
@@ -87,46 +87,54 @@ $moksleiviai =[
     body {
         height: 100%;
     }
-
     body {
         margin: 0;
+        background: -webkit-linear-gradient(45deg, #49a09d, #5f2c82);
         background: linear-gradient(45deg, #49a09d, #5f2c82);
         font-family: sans-serif;
         font-weight: 100;
     }
-
     .container {
         position: absolute;
         top: 50%;
         left: 50%;
+        -webkit-transform: translate(-50%, -50%);
         transform: translate(-50%, -50%);
     }
-
     table {
         width: 800px;
         border-collapse: collapse;
         overflow: hidden;
-        box-shadow: 0 0 20px rgba(0,0,0,0.1);
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
     }
-
     th,
     td {
-        text-align: center;
         padding: 15px;
-        background-color: rgba(255,255,255,0.2);
+        background-color: rgba(255, 255, 255, 0.2);
         color: #fff;
     }
-
     th {
-        text-align: center;
+        text-align: left;
     }
-
-    thead {
-    th {
+    thead th {
         background-color: #55608f;
     }
+    tbody tr:hover {
+        background-color: rgba(255, 255, 255, 0.3);
     }
-
+    tbody td {
+        position: relative;
+    }
+    tbody td:hover:before {
+        content: "";
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: -9999px;
+        bottom: -9999px;
+        background-color: rgba(255, 255, 255, 0.2);
+        z-index: -1;
+    }
 
 </style>
 
@@ -136,53 +144,54 @@ $moksleiviai =[
 
 
 <div class="container">
-<table>
-    <thead>
-    <colgroup span="6"></colgroup>
-    <tr>
-        <th>Klasė</th>
-        <th>Kodas</th>
-        <th>Vardas</th>
-        <th>Pavardė</th>
-        <th>Kontrolinių darbų vidurkis</th>
-        <th>Duomenu formatavimo data</th>
-    </tr>
-    </thead>
+    <table>
+        <thead>
+        <colgroup span="6"></colgroup>
+        <tr>
+            <th>Klasė</th>
+            <th>Kodas</th>
+            <th>Vardas</th>
+            <th>Pavardė</th>
+            <th>Kontrolinių darbų vidurkis</th>
+            <th>Duomenu formatavimo data</th>
+        </tr>
+        </thead>
 
 
 
 
-    <?php $t=0; $f=0;?>
-    <?php foreach ($moksleiviai as $klase => $pirmas):?>
-    <?php foreach ($pirmas as $kodas => $vardai):?>
+        <?php $t=0; ?>
+        <?php foreach ($moksleiviai as $klase => $pirmas):?>
+        <?php foreach ($pirmas as $kodas => $vardai):?>
+        <?php $f=0; ?>
 
 
-    <tbody>
-    <tr>
+        <tbody>
+        <tr>
 
-        <td><?= $klase; ?></td>
-        <td><?= $kodas; ?></td>
-        <? for ($i = 0;
-                $i < 2;
-                $i++){ ?>
-        <td><?= $vardai[$i];
-            } ?> </td>
-        <?php foreach ($vardai["ivertinimai"] as $koda => $rezultatai): ?>
-            <?php $Rezultatai[$f] = $rezultatai;
-            $f = $f + 1; ?>
+            <td><?= $klase; ?></td>
+            <td><?= $kodas; ?></td>
+            <? for ($i = 0;
+                    $i < 2;
+                    $i++){ ?>
+            <td><?= $vardai[$i];
+                } ?> </td>
+            <?php foreach ($vardai["ivertinimai"] as $koda => $rezultatai): ?>
+                <?php $Rezultatai[$f] = $rezultatai;
+                $f = $f + 1; ?>
 
-        <?php endforeach;
-        $foo = ($Rezultatai[0] + $Rezultatai[1] + $Rezultatai[2]) / 3; ?>
-        <td><?= (number_format((float)$foo, 1, '.', '')) ?></td>
+            <?php endforeach;
+            $foo = ($Rezultatai[0] + $Rezultatai[1] + $Rezultatai[2]) / 3; ?>
+            <td><?= (number_format((float)$foo, 1, '.', '')) ?></td>
 
-        <td><?= date("Y-m-d"); ?></td>
-        <?php endforeach; ?>
-        <?php endforeach; ?>
-
-
-    </tr>
-    </tbody>
+            <td><?= date("Y-m-d"); ?></td>
+            <?php endforeach; ?>
+            <?php endforeach; ?>
 
 
-</table>
-    </div>
+        </tr>
+        </tbody>
+
+
+    </table>
+</div>
